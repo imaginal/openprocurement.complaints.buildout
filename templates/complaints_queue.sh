db9b95f5a601387577b647457cc3fa2f3cdd2568
Volodymyr Flonts <vflonts+git@gmail.com>
2016-03-07 20:26:41 +0200

    change basedir

--- templates/complaints_queue.sh
+++ templates/complaints_queue.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+BASEDIR=${buildout:directory}
+
 while true
 do
-  ${buildout:directory}/bin/complaints_queue \
-    ${buildout:directory}/etc/complaints_queue.ini \
-    1>>${buildout:directory}/var/log/queue.stdout.log \
-    2>>${buildout:directory}/var/log/queue.stderr.log
+  $BASEDIR/bin/complaints_queue \
+    $BASEDIR/etc/complaints_queue.ini \
+    1>>$BASEDIR/var/log/queue.stdout.log \
+    2>>$BASEDIR/var/log/queue.stderr.log
   sleep 10
 done
